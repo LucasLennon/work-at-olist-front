7a638da3669093eb1e6f252160a25a35e4fcdc59
unknown <lucas.esperidiao@snowmanlabs.com>
2019-10-12 13:52:11 -0300

    Add simple rules to control form validation

--- src/core/utils/formValidation.js
+++ src/core/utils/formValidation.js
@@ -1,11 +1,11 @@
 export function atLeastSixCharacters(password) {
-    
+    return password.length >= 6;
 }
 
 export function atLeastOneUpperCase(password) {
-
+    return password.match(/([A-Z])+/g).length > 0;
 }
 
 export function atLeastOneNumber(password) {
-    
+    return password.match(/([\d])+/g).length > 0;
 }
