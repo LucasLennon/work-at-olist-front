e75bbc1883316dbc328e26e2dd62c8292bb421fe
unknown <lucas.esperidiao@snowmanlabs.com>
2019-10-15 22:10:37 -0300

    Add a small validation and data entry on RegistrationForm

--- src/core/components/registration/registrationForm.js
+++ src/core/components/registration/registrationForm.js
@@ -1,3 +1,8 @@
+import {
+    atLeastSixCharacters,
+    atLeastOneUpperCase,
+    atLeastOneNumber
+} from '../../utils/formValidation'
 const html = `
     <form>
         <slot></slot>
@@ -8,8 +13,8 @@ const style = `
     form {
         display: grid;
         grid-gap: 24px;
-        justify-items: center;
         justify-content: center;
+        grid-template-columns: minmax(300px, 375px);
     }
 </style>`;
 const template = document.createElement('template');
@@ -20,18 +25,62 @@ template.innerHTML = `
 class RegistrationFormComponent extends HTMLElement {
     constructor() {
         super();
-        this.setData()
+        // this.setData()
+        this._name = null;
+        this._email = null;
+        this._password = null;
+        this._confirmPassword = null;
+
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
-    setData(){
-        return this.data = {
-            name: 'null',
-            email: null,
-            password: null,
-            confirmPassword: null,
+    get formReady() {
+        if (
+        !!this._name &&
+        !!this._email &&
+        !!this._password &&
+        !!this._confirmPassword &&
+        atLeastSixCharacters(this._password) &&
+        atLeastOneUpperCase(this._password) &&
+        atLeastOneNumber(this._password)
+        ) {
+            return true
+        }
+        return false
+    }
+    get formData() {
+        return {
+            name: this._name,
+            email: this._email,
+            password: this._password,
+            confirmPassword: this._confirmPassword,
         };
     }
+    get name() {
+        return this._name;
+    }
+    get email() {
+        return this._email;
+    }
+    get password() {
+        return this._password;
+    }
+    get confirmPassword() {
+        return this._confirmPassword;
+    }
+    set name(value) {
+        this._name = value
+    }
+    set email(value) {
+        this._email = value
+    }
+    set password(value) {
+        this._password = value
+    }
+    set confirmPassword(value) {
+        this._confirmPassword = value
+    }
+
     changeData(value){
         this.data = Object.assign(this.data, value)
     }
